d3af2be74b547be9a43c6c8c89ff2fa4ca5fb504
Marco Peereboom <marco@conformal.com>
2009-01-22 23:12:27 +0000

    Add linux goo; from NicM

---
+++ linux/util.h
@@ -0,0 +1,15 @@
+/* $scrotwm$ */
+
+#define FPARSELN_UNESCESC	0x01
+#define FPARSELN_UNESCCONT	0x02
+#define FPARSELN_UNESCCOMM	0x04
+#define FPARSELN_UNESCREST	0x08
+#define FPARSELN_UNESCALL	0x0f
+
+size_t	strlcpy(char *, const char *, size_t);
+size_t	strlcat(char *, const char *, size_t);
+
+char   *fgetln(FILE *, size_t *);
+char   *fparseln(FILE *, size_t *, size_t *, const char [3], int);
+
+long long strtonum(const char *, long long, long long, const char **);
