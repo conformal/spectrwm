5434fc5516aa64039e53885ca62d56858f76921a
Ryan McBride <mcbride@countersiege.com>
2009-02-11 07:17:06 +0000

    Rework the sample baraction.sh to use iostat -C rather than the sleep as its timer, printing CPU stats as well as APM status.

--- baraction.sh
+++ baraction.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-# $scrotwm$
+
+_print_cpu() {
+	typeset -R4 _1=${1} _2=${2} _3=${3} _4=${4} _5=${5}
+	echo -n "CPU:${_1}% User${_2}% Nice${_3}% Sys${_4}% Int${_5}% Idle     "
+}
+
 print_cpu() {
-	echo -n "CPU: ${7}% User  ${8}% Nice  ${9}% Sys  ${10}% Int  ${11}% Idle     "
+	OUT=""
+	# iostat prints each column justified to 3 chars, so if one counter
+	# is 100, it jams up agains the preceeding one. sort this out.
+	while [ "${1}x" != "x" ]; do
+		if [ ${1} -gt 99 ]; then
+			OUT="$OUT ${1%100} 100"
+		else
+			OUT="$OUT ${1}"
+		fi
+		shift;
+	done
+	_print_cpu $OUT
 }
 
 print_apm() {
@@ -45,9 +61,20 @@ print_apm() {
 }
 
 while :; do
-	# you probably want to reduce the sleep below if you enable this
-	#print_cpu `/usr/sbin/iostat -C`
-	print_apm `/usr/sbin/apm -alb`
-	echo ""
-	sleep 59
+	# instead of sleeping, use iostat as the update timer.
+	# cache the output of apm(8), no need to call that every second.
+	/usr/sbin/iostat -C -c 3600 |&	# wish infinity was an option
+	APM_DATA=""
+	I=0
+	while read -p; do
+		if [ $(( ${I} % 59 )) -eq 0 ]; then
+			APM_DATA=`/usr/sbin/apm -alb`
+		fi
+		if [ $I -gt 2 ]; then
+			print_cpu $REPLY
+			print_apm $APM_DATA
+			echo ""
+		fi
+		I=$(( ${I} + 1 ));
+	done
 done
