26bb3f5b3a491b120e93c1c2225dc620210d449d
Marco Peereboom <marco@conformal.com>
2009-10-07 03:19:11 +0000

    Man page fixes and italian page from: Andrea Bolognani <eof@kiyuko.org>

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 
 PREFIX=scrotwm-
 DIRS="lib linux"
-FILES="Makefile baraction.sh initscreen.sh screenshot.sh scrotwm.1 scrotwm_es.1 scrotwm.c scrotwm.conf linux/Makefile linux/linux.c linux/util.h lib/Makefile lib/shlib_version lib/swm_hack.c"
+FILES="Makefile baraction.sh initscreen.sh screenshot.sh scrotwm.1 scrotwm_es.1 scrotwm_it.1 scrotwm.c scrotwm.conf linux/Makefile linux/linux.c linux/util.h lib/Makefile lib/shlib_version lib/swm_hack.c"
 
 if [ -z "$1" ]; then
 	echo "usage: release.sh <version>"
