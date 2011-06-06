3403d0e9a98d3f739df54c6c4a74d0a578a581ed
Marco Peereboom <marco@conformal.com>
2011-06-06 19:54:48 +0000

    add jason to copyright and update ryan's.

--- scrotwm.c
+++ scrotwm.c
@@ -1,9 +1,10 @@
 /* $scrotwm$ */
 /*
- * Copyright (c) 2009-2010 Marco Peereboom <marco@peereboom.us>
- * Copyright (c) 2009 Ryan McBride <mcbride@countersiege.com>
+ * Copyright (c) 2009-2010-2011 Marco Peereboom <marco@peereboom.us>
+ * Copyright (c) 2009-2010-2011 Ryan McBride <mcbride@countersiege.com>
  * Copyright (c) 2009 Darrin Chandler <dwchandler@stilyagin.com>
  * Copyright (c) 2009 Pierre-Yves Ritschard <pyr@spootnik.org>
+ * Copyright (c) 2011 Jason L. Wright <jason@thought.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
