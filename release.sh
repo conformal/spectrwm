8cc48b14f9a97003819c9b3ca086a7a746b34fe2
Marco Peereboom <marco@conformal.com>
2011-06-23 03:05:04 +0000

    add scrotwm.desktop

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 
 PREFIX=scrotwm-
 DIRS="lib linux osx"
-FILES="Makefile baraction.sh initscreen.sh screenshot.sh scrotwm.1 scrotwm_es.1 scrotwm_it.1 scrotwm_pt.1 scrotwm_ru.1 scrotwm.c scrotwm.conf linux/Makefile linux/linux.c linux/util.h lib/Makefile lib/shlib_version lib/swm_hack.c osx/Makefile osx/osx.h osx/osx.c"
+FILES="Makefile baraction.sh initscreen.sh screenshot.sh scrotwm.1 scrotwm_es.1 scrotwm_it.1 scrotwm_pt.1 scrotwm_ru.1 scrotwm.c scrotwm.conf linux/Makefile linux/linux.c linux/util.h linux/scrotwm.desktop lib/Makefile lib/shlib_version lib/swm_hack.c osx/Makefile osx/osx.h osx/osx.c"
 
 if [ -z "$1" ]; then
 	echo "usage: release.sh <version>"
