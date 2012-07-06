ee9d31bb5d0018e86b8db6a48931bcc8ef050e70
David Hill <dhill@conformal.com>
2012-07-06 00:22:58 -0400

    convert a XGetWindowAttributes

--- spectrwm.c
+++ spectrwm.c
@@ -3223,7 +3223,6 @@ adjust_font(struct ws_win *win)
 void
 stack_master(struct workspace *ws, struct swm_geometry *g, int rot, int flip)
 {
-	XWindowAttributes	wa;
 	struct swm_geometry	win_g, r_g = *g;
 	struct ws_win		*win, *fs_win = NULL;
 	int			i, j, s, stacks;
@@ -3232,6 +3231,8 @@ stack_master(struct workspace *ws, struct swm_geometry *g, int rot, int flip)
 	int			split, colno, winno, mwin, msize, mscale;
 	int			remain, missing, v_slice, reconfigure;
 	int			bordered = 1;
+	xcb_get_window_attributes_cookie_t	wac;
+	xcb_get_window_attributes_reply_t	*war;
 
 	DNPRINTF(SWM_D_STACK, "stack_master: workspace: %d, rot: %s, "
 	    "flip: %s\n", ws->idx, YESNO(rot), YESNO(flip));
@@ -3394,10 +3395,13 @@ stack_master(struct workspace *ws, struct swm_geometry *g, int rot, int flip)
 			update_window(win);
 		}
 
-		if (XGetWindowAttributes(display, win->id, &wa))
-			if (wa.map_state == IsUnmapped)
+		wac = xcb_get_window_attributes(conn, win->id);
+		war = xcb_get_window_attributes_reply(conn, wac, NULL);
+		if (war) {
+			if (war->map_state == XCB_MAP_STATE_UNMAPPED)
 				map_window_raised(win->id);
-
+			free(war);
+		}
 		last_h = win_g.h;
 		i++;
 		j++;
