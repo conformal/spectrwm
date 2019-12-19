conformal/spectrwm

commit 3356f1ccd4f323baa9accda99795d5377f9a24ba

Update bar more often and remove ./ from baraction.sh

baraction.sh
@@ -76,7 +76,7 @@ while :; do
 	APM_DATA=""
 	I=0
 	while read -p; do
-		if [ $(( ${I} % 59 )) -eq 0 ]; then
+		if [ $(( ${I} % 1 )) -eq 0 ]; then
 			APM_DATA=`/usr/sbin/apm -alb`
 		fi
 		if [ $I -gt 2 ]; then

scrotwm.conf
@@ -10,7 +10,7 @@ bar_border[1]		= rgb:00/80/80
 bar_color[1]		= black
 bar_font_color[1]	= rgb:a0/a0/a0
 bar_font		= -*-terminus-medium-*-*-*-*-*-*-*-*-*-*-*
-#bar_action		= ./baraction.sh
+#bar_action		= baraction.sh
 #bar_delay		= 1
 clock_enabled		= 1
 

commit 7c3f31a242a34a7a4c5a4913c13c48fb020a8657

baraction: Fix apm data caching
The idea is that it's pointless to collect information about the
battery every single second, so caching has been implemented for
the output of apm; however, the condition has been broken since
3356f1ccd4f3, which resulted in data being collected every single
second after all.

This fixes the check so that information is collected every 11
seconds, which fits nicely into the 22 second long cycle used to
parse iostat's output.

baraction.sh
@@ -90,7 +90,7 @@ while :; do
 	I=0
 	trap "kill $PID; exit" TERM
 	while read -p; do
-		if [ $(( ${I} % 1 )) -eq 0 ]; then
+		if [ $(( ${I} % 11 )) -eq 0 ]; then
 			APM_DATA=`/usr/sbin/apm -alb`
 		fi
 		if [ $I -ge 2 ]; then