5e78da86cda36ccb4d4736dacf37fff2c5a3f62f
Björn Ketelaars <bjorn.ketelaars@hydroxide.nl>
2021-01-24 13:24:05 +0100

    Switch from SIMPLEQ- to STAILQ-macros
    OpenBSD added singly-linked tail queue macros. Switching to these macros
    has as advantage that the compatibility goo can be simplified.
    
    Build tested on OpenBSD 6.8, OpenBSD current, Linux (Void), FreeBSD 12.2
    and macOS 11.0.1. Run tested on OpenBSD current.

---
+++ linux/queue_compat.h
@@ -0,0 +1,11 @@
+#ifndef TAILQ_END
+#define	TAILQ_END(head)			NULL
+#endif
+
+#ifndef TAILQ_FOREACH_SAFE
+#define	TAILQ_FOREACH_SAFE(var, head, field, tvar)			\
+	for ((var) = TAILQ_FIRST(head);					\
+	    (var) != TAILQ_END(head) &&					\
+	    ((tvar) = TAILQ_NEXT(var, field), 1);			\
+	    (var) = (tvar))
+#endif
--- linux/util.h
+++ linux/util.h
@@ -15,32 +15,3 @@ long long strtonum(const char *, long long, long long, const char **);
 #ifndef WAIT_ANY
 #define WAIT_ANY		(-1)
 #endif
-
-/* there is no limit to ulrich drepper's crap */
-#ifndef TAILQ_END
-#define	TAILQ_END(head)			NULL
-#endif
-
-#ifndef TAILQ_FOREACH_SAFE
-#define	TAILQ_FOREACH_SAFE(var, head, field, tvar)			\
-	for ((var) = TAILQ_FIRST(head);					\
-	    (var) != TAILQ_END(head) &&					\
-	    ((tvar) = TAILQ_NEXT(var, field), 1);			\
-	    (var) = (tvar))
-#endif
-
-#ifndef SIMPLEQ_HEAD
-#define SIMPLEQ_HEAD                    STAILQ_HEAD
-#define SIMPLEQ_HEAD_INITIALIZER        STAILQ_HEAD_INITIALIZER
-#define SIMPLEQ_ENTRY                   STAILQ_ENTRY
-#define SIMPLEQ_INIT                    STAILQ_INIT
-#define SIMPLEQ_INSERT_AFTER            STAILQ_INSERT_AFTER
-#define SIMPLEQ_INSERT_HEAD             STAILQ_INSERT_HEAD
-#define SIMPLEQ_INSERT_TAIL             STAILQ_INSERT_TAIL
-#define SIMPLEQ_EMPTY                   STAILQ_EMPTY
-#define SIMPLEQ_FIRST                   STAILQ_FIRST
-#define SIMPLEQ_REMOVE_AFTER            STAILQ_REMOVE_AFTER
-#define SIMPLEQ_REMOVE_HEAD             STAILQ_REMOVE_HEAD
-#define SIMPLEQ_FOREACH                 STAILQ_FOREACH
-#define SIMPLEQ_END(head)               NULL
-#endif
