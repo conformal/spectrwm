c8302056854b448f9da7117a91ac6b71f165c72d
Reginald Kennedy <rk@rejii.com>
2024-05-28 22:22:06 +0800

    linux: remove BSD function ports and depend on `libbsd`
    Resolves a memory leak and helps avoid porting issues.

--- linux/util.h
+++ linux/util.h
@@ -1,17 +1,3 @@
-#define FPARSELN_UNESCESC	0x01
-#define FPARSELN_UNESCCONT	0x02
-#define FPARSELN_UNESCCOMM	0x04
-#define FPARSELN_UNESCREST	0x08
-#define FPARSELN_UNESCALL	0x0f
-
-size_t	strlcpy(char *, const char *, size_t);
-size_t	strlcat(char *, const char *, size_t);
-
-char   *fgetln(FILE *, size_t *);
-char   *fparseln(FILE *, size_t *, size_t *, const char [3], int);
-
-long long strtonum(const char *, long long, long long, const char **);
-
 #ifndef WAIT_ANY
 #define WAIT_ANY		(-1)
 #endif
